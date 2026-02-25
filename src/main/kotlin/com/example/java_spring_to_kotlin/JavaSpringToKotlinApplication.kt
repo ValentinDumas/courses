385444ae02ebb89ffe790eed3033d195beae8226
vdumas <pro.valentindumas@gmail.com>
2026-02-25 22:16:42 +0100

    Init project.

---
+++ src/main/kotlin/com/example/java_spring_to_kotlin/JavaSpringToKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.java_spring_to_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaSpringToKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaSpringToKotlinApplication>(*args)
+}
